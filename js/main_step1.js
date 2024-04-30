d0a4e67c58017e3fc8bb5816a52265a2a6fa5181
jiweihong0 <jiweihong0@gmail.com>
2024-04-30 20:01:26 +0800

    feat: part 1 on gltf

--- js/main_step1.js
+++ js/main_step1.js
@@ -85,69 +85,39 @@ function createLights() {
   scene.add(shadowLight);
 }
 
-
+var sea;
+var airplane;
+var mixer; // Add this line
+var clock = new THREE.Clock(); 
 var AirPlane = function(){
-	this.mesh = new THREE.Object3D();
-  this.mesh.name = "airPlane";
-
-  // Create the cabin
-	var geomCockpit = new THREE.BoxGeometry(60,50,50,1,1,1);
-  var matCockpit = new THREE.MeshPhongMaterial({color:Colors.red, shading:THREE.FlatShading});
-  var cockpit = new THREE.Mesh(geomCockpit, matCockpit);
-	cockpit.castShadow = true;
-  cockpit.receiveShadow = true;
-  this.mesh.add(cockpit);
-
-  // Create Engine
-  var geomEngine = new THREE.BoxGeometry(20,50,50,1,1,1);
-  var matEngine = new THREE.MeshPhongMaterial({color:Colors.white, shading:THREE.FlatShading});
-  var engine = new THREE.Mesh(geomEngine, matEngine);
-  engine.position.x = 40;
-  engine.castShadow = true;
-  engine.receiveShadow = true;
-	this.mesh.add(engine);
-
-  // Create Tailplane
-
-  var geomTailPlane = new THREE.BoxGeometry(15,20,5,1,1,1);
-  var matTailPlane = new THREE.MeshPhongMaterial({color:Colors.red, shading:THREE.FlatShading});
-  var tailPlane = new THREE.Mesh(geomTailPlane, matTailPlane);
-  tailPlane.position.set(-35,25,0);
-  tailPlane.castShadow = true;
-  tailPlane.receiveShadow = true;
-	this.mesh.add(tailPlane);
-
-  // Create Wing
-
-  var geomSideWing = new THREE.BoxGeometry(40,8,150,1,1,1);
-  var matSideWing = new THREE.MeshPhongMaterial({color:Colors.red, shading:THREE.FlatShading});
-  var sideWing = new THREE.Mesh(geomSideWing, matSideWing);
-  sideWing.position.set(0,0,0);
-  sideWing.castShadow = true;
-  sideWing.receiveShadow = true;
-	this.mesh.add(sideWing);
-
-  // Propeller
-
-  var geomPropeller = new THREE.BoxGeometry(20,10,10,1,1,1);
-  var matPropeller = new THREE.MeshPhongMaterial({color:Colors.brown, shading:THREE.FlatShading});
-  this.propeller = new THREE.Mesh(geomPropeller, matPropeller);
-  this.propeller.castShadow = true;
-  this.propeller.receiveShadow = true;
-
-  // Blades
-
-  var geomBlade = new THREE.BoxGeometry(1,100,20,1,1,1);
-  var matBlade = new THREE.MeshPhongMaterial({color:Colors.brownDark, shading:THREE.FlatShading});
-
-  var blade = new THREE.Mesh(geomBlade, matBlade);
-  blade.position.set(8,0,0);
-  blade.castShadow = true;
-  blade.receiveShadow = true;
-	this.propeller.add(blade);
-  this.propeller.position.set(50,0,0);
-  this.mesh.add(this.propeller);
+  this.mesh = new THREE.Object3D(); // Add this line
+  var loader = new THREE.GLTFLoader();
+  var self = this;
+  loader.load(
+    '../models/bird_flight_animation/scene.gltf',
+    function ( gltf ) {
+      var model = gltf.scene;
+      model.scale.set(0.5,0.5,0.5);
+      model.position.y = 100;
+      model.rotation.y = Math.PI/2;
+      model.castShadow = true;
+      model.receiveShadow = true;
+      // animation
+      mixer = new THREE.AnimationMixer( model );
+      var action = mixer.clipAction( gltf.animations[ 0 ] );
+      action.play();
+      self.mesh.add( model ); // Change this line
+      gltf.animations; // Array<THREE.AnimationClip>
+    },
+    function ( xhr ) {
+      console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+    },
+    function ( error ) {
+      console.log( 'An error happened' );
+    }
+  );
 };
+
 
 Sky = function(){
   this.mesh = new THREE.Object3D();
@@ -212,8 +182,9 @@ var airplane;
 
 function createPlane(){
   airplane = new AirPlane();
-  airplane.mesh.scale.set(.25,.25,.25);
-  airplane.mesh.position.y = 100;
+  
+  
+  
   scene.add(airplane.mesh);
 }
 
@@ -233,17 +204,24 @@ function loop(){
   updatePlane();
   sea.mesh.rotation.z += .005;
   sky.mesh.rotation.z += .01;
+  if (mixer) mixer.update(clock.getDelta()); // Add this line
   renderer.render(scene, camera);
   requestAnimationFrame(loop);
 }
 
 function updatePlane(){
+  if (!mousePos) {
+    return;
+  }
+
   var targetY = normalize(mousePos.y,-.75,.75,25, 175);
   var targetX = normalize(mousePos.x,-.75,.75,-100, 100);
   airplane.mesh.position.y = targetY;
   airplane.mesh.position.x = targetX;
-  airplane.propeller.rotation.x += 0.3;
+  // airplane.propeller.rotation.x += 0.3;
+
 }
+
 
 function normalize(v,vmin,vmax,tmin, tmax){
   var nv = Math.max(Math.min(v,vmax), vmin);
